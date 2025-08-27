3dacb2d290b3edc50c9251612c1737bd297ae8df
maaz-codes <maazkhan7454@gmail.com>
2025-08-27 17:25:59 +0400

    add action parsing and tool execution in the re-act step

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from dotenv import load_dotenv
+from typing import List, Union
+
 from langchain.tools import tool
 from langchain.prompts import PromptTemplate
 from langchain_core.tools.render import render_text_description
 from langchain_openai import ChatOpenAI
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain.tools import Tool
+from langchain.agents.output_parsers.react_single_input import ReActSingleInputOutputParser
 
 
 load_dotenv()
@@ -14,6 +19,12 @@ def get_text_length(text: str) -> int:
 
     text = text.strip("'\n").strip('"')
     return len(text)
+
+def get_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found!")
 
 
 def main():
@@ -39,17 +50,27 @@ def main():
         Begin!
 
         Question: {input}
-        Thought:{agent_scratchpad}
+        Thought:
     """ 
+
     prompt = PromptTemplate.from_template(template=template).partial(
         tools=render_text_description(tools), 
         tool_names=", ".join([t.name for t in tools]),
     )
-
-    llm = ChatOpenAI(temperature=0, model='gpt-4o-mini', stop='\nObservation')
  
-     
+    llm = ChatOpenAI(temperature=0, model='gpt-4o-mini', stop=['\nObservation:'])
+    agent = {"input": lambda x: x['input']} | prompt | llm | ReActSingleInputOutputParser()
+    query = "What is the text length of the word 'DOG' in characters?"
+    
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input": query})
 
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = get_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.func(str(tool_input))
+        print(f"Observation: {observation}")
 
 
 if __name__ == "__main__":
