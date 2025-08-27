fea0145c7679d7a71affd12d9274527b08c49f11
maaz-codes <maazkhan7454@gmail.com>
2025-08-27 15:58:42 +0400

    basic setup for react-agent

---
+++ main.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+from langchain.tools import tool
+from langchain.prompts import PromptTemplate
+from langchain_core.tools.render import render_text_description
+from langchain_openai import ChatOpenAI
+
+
+load_dotenv()
+
+
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the length of a text by characters"""
+
+    text = text.strip("'\n").strip('"')
+    return len(text)
+
+
+def main():
+    print("Hello from langChain!")
+
+    tools = [get_text_length]
+    template = """
+        Answer the following questions as best you can. You have access to the following tools:
+
+        {tools}
+
+        Use the following format:
+
+        Question: the input question you must answer
+        Thought: you should always think about what to do
+        Action: the action to take, should be one of [{tool_names}]
+        Action Input: the input to the action
+        Observation: the result of the action
+        ... (this Thought/Action/Action Input/Observation can repeat N times)
+        Thought: I now know the final answer
+        Final Answer: the final answer to the original input question
+
+        Begin!
+
+        Question: {input}
+        Thought:{agent_scratchpad}
+    """ 
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools), 
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+    llm = ChatOpenAI(temperature=0, model='gpt-4o-mini', stop='\nObservation')
+ 
+     
+
+
+
+if __name__ == "__main__":
+    main()
