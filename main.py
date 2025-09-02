e9241c3d696176ca072aaddcd1904aac6f9fa674
maaz-codes <maazkhan7454@gmail.com>
2025-09-02 19:25:19 +0400

    made router agent a function calling agent

--- main.py
+++ main.py
@@ -7,31 +7,35 @@ from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain.agents import AgentExecutor
 from agents import code_agent, csv_agent
+from langchain.agents.tool_calling_agent.base import create_tool_calling_agent
+from langchain_core.prompts.chat import ChatPromptTemplate
 from lc_logger import MyLogger
 
 
 load_dotenv()
+
 
 def code_agent_wrapper(prompt: str) -> dict[str, Any]:
     return code_agent.invoke(input={"input": prompt})
 
 
 def main():
-    print("Hello from Code Interpreter!")
+    print("Hello from Router Agent!")
 
 
     tools = [
         Tool(
-            name="Python Agent",
+            name="python_agent",
             func=code_agent_wrapper,
             description="""
-                useful when you need to transform natural language to python and execute the python code,
-                returning the results of the code execution.
-                DOES NOT ACCEPT CODE AS INPUT.
+                Takes ONLY natural language requests and converts them to Python code internally.
+                Input should be plain English like: 'generate a code for calculator app'
+                DO NOT pass Python code - pass the user's original natural language request.
+                This tool will handle the code generation and execution internally.
             """
         ),
         Tool(
-            name="CSV Agent",
+            name="csv_agent",
             func=csv_agent.invoke,
             description="""
                 useful when you need to answer question over seinfield.csv file,
@@ -44,10 +48,24 @@ def main():
     prompt = base_prompt.partial(instructions="")
     llm = ChatOpenAI(model='gpt-4o', temperature=0)
 
+    tool_calling_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "you're a helpful assistant"),
+            ("human", "{input}"),
+            ("placeholder", "{agent_scratchpad}"),
+        ]
+    )
+
     my_logger = MyLogger()
 
-    create_router_agent = create_react_agent(
-        prompt=prompt,
+    # create_router_agent = create_react_agent(
+    #     prompt=prompt,
+    #     llm=llm,
+    #     tools=tools,
+    # )
+
+    create_router_agent = create_tool_calling_agent(
+        prompt=tool_calling_prompt,
         llm=llm,
         tools=tools,
     )
@@ -56,7 +74,7 @@ def main():
         agent=create_router_agent, 
         tools=tools, 
         verbose=True,
-        callbacks=[my_logger]
+        # callbacks=[my_logger]
     )
 
     # res = router_agent.invoke(input={"input": "Which season has the most episodes?"})
