25aa50fa5c9db8044e662c58ae25aaff90290ca1
maaz-codes <maazkhan7454@gmail.com>
2025-09-17 17:45:00 +0400

    created tests for retreival chain

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,2 +1,29 @@
-def test_foo() -> None:
-    assert 1 == 1
+from dotenv import load_dotenv
+from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from ingestion_pinecone import retriver
+
+
+load_dotenv()
+
+
+def test_retrieval_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriver.invoke(question)
+    doc_txt = docs[0].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "yes"
+
+def test_retrieval_grader_answer_no() -> None:
+    question = "making pizza"
+    docs = retriver.invoke(question)
+    doc_txt = docs[0].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "no"
